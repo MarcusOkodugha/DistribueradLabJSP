218ea980c6880c0cd823614728f1e2f6fe21c750
Hamada Aljarrah <hamadasepost@gmail.com>
2023-10-02 15:47:28 +0200

    test update

--- src/main/java/com/example/distribueradlab/HelloServlet.java
+++ src/main/java/com/example/distribueradlab/HelloServlet.java
@@ -10,7 +10,7 @@ public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "Hello World!";
+        message = "Hello World after Hamada updated!";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
